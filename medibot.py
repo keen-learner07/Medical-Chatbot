9753cfc17c6834d56efa1fd0017327a8a5ceb429
keen-learner07 <duakhushi08@gmail.com>
2025-10-18 19:34:56 +0530

    Removed unused dependency

--- medibot.py
+++ medibot.py
@@ -5,7 +5,6 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
-from langchain import hub
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
